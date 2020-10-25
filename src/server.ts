283f14eafc995781d22056b1d3cfd2e898e9336a
Alisson Boucinhas <alissonb.dev@gmail.com>
2020-10-25 02:01:49 -0300

    feat: create orphanage

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,41 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
 
 import './database/connection';
+import Orphanage from './models/orphanage';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-    return response.json({ message: 'hello world' });
+app.post('/orphanages', async (request, response) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about, 
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body;
+
+    var repository = getRepository(Orphanage);
+
+    const orphanage = repository.create({
+        name,
+        latitude,
+        longitude,
+        about, 
+        instructions,
+        opening_hours,
+        open_on_weekends
+    });
+
+    await repository.save(orphanage);
+
+    return response
+            .status(201)
+            .json(orphanage);
 });
 
 app.listen(3333);
